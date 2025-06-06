284fe080896721bb8bea10ac7f4416f54699aedb
Gili Yaacobi <gili.yaacobi@similarweb.com>
2025-06-06 16:23:17 +0300

    remove debug log

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -153,7 +153,6 @@ function Home() {
     setDropdownDisabled(true);
     setTrueClass('');
     setShowSavedMessage(true);
-    console.log("got here");
     handleStart(false);
     setTimeout(() => setShowSavedMessage(false), 7600);
   
